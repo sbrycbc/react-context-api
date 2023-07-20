c1123865384874123045cc27fa15bbb160056ae9
sbrycbc <sabriyecbc@gmail.com>
2023-07-20 15:14:49 +0200

    added contexts...

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,35 +1,34 @@
 import React from "react";
-import './BookList.css';
-import Book from './Book';
+import "./BookList.css";
+import Book from "./Book";
+import  BookContext  from "../contexts/BookContext";
 
 class BookList extends React.Component {
+  static contextType = BookContext;
 
-    render() {
-        
-        // console.log(this.props.books)
+  render() {
+    // console.log(this.props.books)
 
-        const bookList = this.props.books.map((book , i) => {
-            return <Book book={book}
-                         key={i}
-                         />
-        })
-            
-            return (
-                <section className="page-section bg-light" id="portfolio">
-                    <div className="container">
-                        <div className="text-center">
-                            <h2 className="section-heading text-uppercase">BookList</h2>
-                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                        </div>
-                        <div className="row">
-                            {bookList}
-                        </div>
-                    </div>
-                </section>
-            )
-        }
+    const books = this.context;
+
+    return (
+      <section className="page-section bg-light" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">BookList</h2>
+            <h3 className="section-subheading text-muted">
+              Lorem ipsum dolor sit amet consectetur.
+            </h3>
+          </div>
+          <div className="row">
+            {books.map((book, i) => {
+              return <Book book={book} key={i} />;
+            })}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
-    
-    
 
 export default BookList;
---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+
+export default  React.createContext(
+    [
+        {      
+          title: "Kirmizi Pazartesi",
+          author: "Gabriel Garcia Marquez",
+          pageNum: 296,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
+          topic: "1968-1970 yillari arasinda geçen olaylar, o günün toplumsal gerçeklerini de satirlara taşiyor. Ama romanin odağinda bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgâriyla hareketlenen İmkânsizin Şarkisini ölümle erken karşilaşan gençlerin hayati yönlendiriyor. Hiçbir şeyin önem taşimadiği, amaçsizliğin ağir bastiği, özgür seksin kol gezdiği bir öğrenci hayati... Ama diğer yanda da yoğun duygular var... İmkânsiz aşklar, imkânsiz şarkilar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dişinda da çok kişi tarafindan sahipleniliyor."
+        },
+        {      
+          title: "Şeker Portakali",
+          author: "Jose Muro de Vasconselos",
+          pageNum: 200,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0000000064031-1.jpg",
+          topic: "İrlandali yazar Bram Stoker’in, iki taraf arasindaki bu irade ve güç çatişmasini işlediği ve korku edebiyatinin başyapitlarindan biri sayilan Dracula, yayimlanmasinin üzerinden yüz yili aşkin süre geçmesine karşin, bugün de ayni ilgiyle okunuyor."
+        },
+        {      
+          title: "En Uzun Yüzyil",
+          author: "İlber Ortayli",
+          pageNum: 296,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001744876001-1.jpg",
+          topic: "1968-1970 yillari arasinda geçen olaylar, o günün toplumsal gerçeklerini de satirlara taşiyor. Ama romanin odağinda bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgâriyla hareketlenen İmkânsizin Şarkisini ölümle erken karşilaşan gençlerin hayati yönlendiriyor. Hiçbir şeyin önem taşimadiği, amaçsizliğin ağir bastiği, özgür seksin kol gezdiği bir öğrenci hayati... Ama diğer yanda da yoğun duygular var... İmkânsiz aşklar, imkânsiz şarkilar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dişinda da çok kişi tarafindan sahipleniliyor."
+        },
+        {      
+          title: "Dracula",
+          author: "Bram Stoker",
+          pageNum: 200,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001828853001-1.jpg",
+          topic: "İrlandali yazar Bram Stoker’in, iki taraf arasindaki bu irade ve güç çatişmasini işlediği ve korku edebiyatinin başyapitlarindan biri sayilan Dracula, yayimlanmasinin üzerinden yüz yili aşkin süre geçmesine karşin, bugün de ayni ilgiyle okunuyor."
+        },
+        {      
+          title: "Karamazov Kardeşler",
+          author: "Fyodor Mihayloviç Dostoyevski",
+          pageNum: 200,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001803800001-1.jpg",
+          topic: "İrlandali yazar Bram Stoker’in, iki taraf arasindaki bu irade ve güç çatişmasini işlediği ve korku edebiyatinin başyapitlarindan biri sayilan Dracula, yayimlanmasinin üzerinden yüz yili aşkin süre geçmesine karşin, bugün de ayni ilgiyle okunuyor."
+        },
+        {      
+          title: "Sultanin Korsanlari",
+          author: "Emrah Safa Gürkan",
+          pageNum: 296,
+          imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
+          topic: "1968-1970 yillari arasinda geçen olaylar, o günün toplumsal gerçeklerini de satirlara taşiyor. Ama romanin odağinda bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgâriyla hareketlenen İmkânsizin Şarkisini ölümle erken karşilaşan gençlerin hayati yönlendiriyor. Hiçbir şeyin önem taşimadiği, amaçsizliğin ağir bastiği, özgür seksin kol gezdiği bir öğrenci hayati... Ama diğer yanda da yoğun duygular var... İmkânsiz aşklar, imkânsiz şarkilar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dişinda da çok kişi tarafindan sahipleniliyor."
+        }
+
+        ]
+);
+
+
+
+
+
